aa5de9d2e82535c60eda310f82e8c8c2fc4e6a45
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-20 14:53:47 +0300

    Install ui lib

---
+++ src/app/core/hooks/useTheme.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Colors, ThemeManager, Typography } from 'react-native-ui-lib';
+
+import { BrandTheme } from '@theme/default';
+
+type UseTheme = {
+    loadTheme: () => Promise<void>;
+    loading: boolean;
+};
+
+export default function useTheme(): UseTheme {
+    const [loading, setLoading] = useState<boolean>(true);
+
+    function setTheme(config: BrandTheme): void {
+        Colors.loadColors(config.colors);
+        Typography.loadTypographies(config.typography);
+
+        // TODO (Integrate in Brand Theme) - @Roma Vizitiu
+        ThemeManager.setComponentTheme('Button', {
+            borderRadius: 12,
+        });
+
+        setLoading(false);
+    }
+
+    async function loadTheme(): Promise<void> {
+        // @ts-ignore
+        const theme = (await import('@theme/default.ts'))
+            .default as BrandTheme;
+        setTheme(theme);
+    }
+
+    return {
+        loadTheme,
+        loading,
+    };
+}
