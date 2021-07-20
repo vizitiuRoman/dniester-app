30e1c29017d3c9f5c1881023fe86bdb8804736fb
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-20 18:41:35 +0300

    chore

--- src/app/shared/constants/apis.ts
+++ src/app/shared/constants/apis.ts
@@ -1,6 +1,18 @@
-export const APIS = {
-    auth: 'auth',
-    booking: 'bookings',
-    service: 'services',
-    company: 'companies',
+export const AUTH_API = {
+    login: 'user/login',
+    register: 'user/register',
+    me: 'user/me',
+};
+
+export const BOOKING_API = {
+    default: 'bookings',
+};
+
+export const SERVICE_API = {
+    default: 'services',
+};
+
+
+export const COMPANY_API = {
+    default: 'companies',
 };
