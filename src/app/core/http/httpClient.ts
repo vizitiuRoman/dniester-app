e87e76074d79cfed0e7ff88f74840148eac6a253
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-20 14:32:38 +0300

    Create axios instance

--- src/app/core/http/httpClient.ts
+++ src/app/core/http/httpClient.ts
@@ -1,5 +1,6 @@
-import { environment } from '@environments/environment';
 import axios, { AxiosInstance } from 'axios';
+
+import { environment } from '@environments/environment';
 
 export default axios.create({
     baseURL: environment.API_URL,
