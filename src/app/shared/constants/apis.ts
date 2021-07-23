d1be7ce88ab50e1e253aaf4328054fc192b061fa
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-23 16:00:59 +0300

    update endpoints

--- src/app/shared/constants/apis.ts
+++ src/app/shared/constants/apis.ts
@@ -1,17 +1,17 @@
 export const AUTH_API = {
-    login: 'user/auth/login',
-    register: 'user/auth/register',
-    me: 'user/auth/me',
+    login: 'app/auth/login',
+    register: 'app/auth/register',
+    me: 'app/auth/me',
 };
 
 export const BOOKING_API = {
-    default: 'bookings',
+    default: 'app/bookings',
 };
 
 export const SERVICE_API = {
-    default: 'services',
+    default: 'app/services',
 };
 
 export const COMPANY_API = {
-    default: 'companies',
+    default: 'app/companies',
 };
